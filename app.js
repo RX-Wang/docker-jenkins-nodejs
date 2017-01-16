5d0571d40e27cf295883411a4ca33720157b29fa
wangxuquan <1002853070@qq.com>
2017-01-16 23:54:44 +0800

    02

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'});
 	console.log('\n',1);
-	res.end('Hello Docker!001');
+	res.end('Hello Docker!002');
 }).listen(3000);
