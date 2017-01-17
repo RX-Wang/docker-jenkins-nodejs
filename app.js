1c26b5bae2cb83c3c10c9dc4d45e6c7a4e677616
wangxuquan <1002853070@qq.com>
2017-01-18 01:14:00 +0800

    bb

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'});
 	console.log('\n',1);
-	res.end('Hello Docker!再次尝试！');
+	res.end('Hello Docker!测试初始化PM2时，也就是初始没有pm2进程，然后进行pm2 stop会不会报错！');
 }).listen(9001);
