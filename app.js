bb29b23c268c086d68c4e60bef537247a8b8f780
wangxuquan <1002853070@qq.com>
2017-01-18 00:53:47 +0800

    bb

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'});
 	console.log('\n',1);
-	res.end('Hello Docker!003');
+	res.end('Hello Docker!自动构建开始！');
 }).listen(9001);
